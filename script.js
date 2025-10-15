e2d7cfcd0b7d505b4457d8907d2c34da2b88dddf
Ic3Sh4rk <louis.pouvreau1@gmail.com>
2025-10-15 10:14:45 +0200

    version telephone

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const header = document.querySelector('header');
+const underlineTitle = document.querySelector(':root');
+
+
 
 function checkScroll() {
   if (window.scrollY === 0) {
@@ -8,7 +11,12 @@ function checkScroll() {
   }
 }
 
+function underlineUpdate2() {
+  underlineTitle.style.setProperty('--underline-height', innerWidth / (innerHeight * 2) + 'vh'); 
+}
 
 
 window.addEventListener('scroll', checkScroll);
 window.addEventListener('load', checkScroll);
+window.addEventListener('load', underlineUpdate)
+window.addEventListener('resize', underlineUpdate)
