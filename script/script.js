ae2682184c10a82f0c078dceb1f15283d44ee8b8
Ic3Sh4rk <louis.pouvreau1@gmail.com>
2025-11-04 10:27:48 +0100

    animation

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 
 const header = document.querySelector('header');
-const underlineTitle = document.querySelector(':root');
+const root = document.querySelector(':root');
+const homeButton = document.querySelector('li.menu.home div');
+const homeText = homeButton.querySelector('div');
+
 
 
 
 function checkScroll() {
-  if (window.scrollY === 0) {
+  if (scrollY === 0) {
     header.classList.add('hidden');
   } else {
     header.classList.remove('hidden');
@@ -13,11 +16,17 @@ function checkScroll() {
 }
 
 function underlineUpdate() {
-  underlineTitle.style.setProperty('--underline-height-bottom', innerWidth / (innerHeight * 2) + 'vh'); 
+  root.style.setProperty('--underline-height-bottom', innerWidth / (innerHeight * 2) + 'vh'); 
 }
 
 function homeButtonUpdate() {
-
+  if (homeButton.clientWidth <= 216.417){
+    homeText.classList.add('shrink');
+    homeText.classList.remove('enlarge');
+  } else {
+    homeText.classList.remove('shrink');
+    homeText.classList.add('enlarge');
+  }
 }
 
 
